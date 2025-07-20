b54cd9d9f84d03d6cb7386987f2cb447b7ad4d4e
spiltcoffee <spiltcoffee@users.noreply.github.com>
2025-07-20 16:39:32 +1000

    Add release script and changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/)
+and this project adheres to [Semantic Versioning](https://semver.org/).
+
+## [Unreleased]
+
+### Added
+
+- Release script
+- Changelog
+
+## [1.0.0] - 2025-07-20
+
+- Initial version
+
+[Unreleased]: https://github.com/yellow-corps/enforcer-handle-validator/compare/v1.0.0...HEAD
+[1.0.0]: https://github.com/yellow-corps/enforcer-handle-validator/releases/tag/v1.0.0
---
+++ release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -o errexit
+
+version="$1"
+
+npm version --no-git-tag-version --allow-same-version "$version"
+
+version="$(node -p "require('./package.json').version")"
+
+npm run format-changelog
+npm run bump-changelog "$version"
+npm run format-changelog
+
+git add CHANGELOG.md package.json package-lock.json
+git commit -m "v$version"
+git tag "v$version"
+git push origin --atomic main "v$version"
+
+changelog="$(npm run --silent get-latest-changelog)"
+
+gh release create "v$version" --title "v$version" --notes "$changelog"
